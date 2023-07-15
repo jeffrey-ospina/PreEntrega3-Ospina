715af70ce5052680931b8be51e2517d6a08499fd
Jeffrey Ospina <jeffos_11@hotmail.com>
2023-07-15 09:10:13 -0400

    se agregan otras funciones al script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PreEntrega3-Ospina
+
+El archivo junto con sus funcionalidades son exclusivamente de prueba para realizar la entrega.
+
+Funcionalidad del programa: La idea es asignar mediante una función aleatoria un número que se convierte en el identificador del usuario. Luego se avanza para poder hacer la consulta de la deuda a pagar usando otra función aleatoria que arroja dicho valor. Se puede pagar el monto donde se aplica un evento a dicho botón.
+
+Los detalles al arrojar el número aleatorio, se guardan en un objeto junto con la fecha y el monto a pagar.
--- js/simulador.js
+++ js/simulador.js
@@ -1,3 +1,21 @@
+//Declaración de variables
+const precioHora = 400;
+
+//Arrow Function para crear valores aleatorios para el pago del servicio
+const valorAleatorio = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+//Función para crear un valor aleatorio
+function mostrarNumeroAleatorio() {
+  let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+  //Se muestra el resultado a través del HTML
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "<h4>Tu número es: #" + numeroAleatorio;
+  botones.style.display = "block";
+  boton.style.display = "none";
+}
+
 //Se obtienen los elementos del DOM
 let boton = document.getElementById("boton");
 let botones = document.getElementById("botones");
@@ -9,18 +27,46 @@ boton.addEventListener("click", function (e) {
   mostrarNumeroAleatorio();
 });
 
-//Función para crear un valor aleatorio
-function mostrarNumeroAleatorio() {
-  let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+//Variables de scope global para generar el resultado
+let hora = valorAleatorio(1, 6);
+let minutos = valorAleatorio(1, 60);
+let precio = (minutos / 60) * precioHora + hora * precioHora;
+let precioTotal = precio.toFixed(2);
 
-  //Se muestra el resultado a través del HTML
+//Variables para luego usar en el objeto
+const fecha = new Date();
+const monto = precioTotal;
+const numeroCliente = numeroAleatorio;
+
+//Objeto
+const cliente = {
+  fecha,
+  monto,
+  numeroCliente,
+};
+
+//Se obtiene elemento del para hacer la consulta de la deuda
+let consulta = document.getElementById("consultar");
+
+//Se agrega un evento a dicho boton
+consulta.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //Se muestra el valor que debe pagar el cliente
   let resultado = document.getElementById("resultado");
-  resultado.innerHTML = "<h4>Tu número es: #" + numeroAleatorio;
+  resultado.innerHTML = "<h4>Tu deuda es: " + precioTotal + "$" + "</h4>";
 
-  //Se continúa con los demás elementos de HTML
-  let boton = document.getElementById("boton");
-  boton.addEventListener("click", function () {
-    botones.style.display = "block";
-    boton.style.display = "none";
-  });
-}
+  console.log(cliente);
+});
+
+//Se obtiene el elemento del DOM para realizar el pago de la deuda
+let pagar = document.getElementById("pagar");
+
+//Se agrega un evento a dicho boton
+pagar.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //Se muestra el valor que debe pagar el cliente
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "<h4>HOLA</h4>";
+});
